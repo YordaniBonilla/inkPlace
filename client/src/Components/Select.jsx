3e84ae149ca8818d26d7bbd2667ba675684a31eb
YordaniBonilla <b.yordani93@gmail.com>
2019-11-17 15:15:28 -0600

    added bootstrap css to select

--- client/src/Components/Select.jsx
+++ client/src/Components/Select.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Select = ({options}) => {
 	return (
-	  <div>
-		<select >
+		<div class="input-group mb-3">
+		<select  class="custom-select" id="inputGroupSelect02">
 		  {options.map((item,i) => {
-		  	return <option key={i}> {item.style} </option>
+		  	return <option selected style={{backgroundColor: 'disabled', backgroundImage: 'none'}} key={i}> {item.style} </option>
 		  })}
 		</select>
-	  </div>
+		</div>
 	)
 }
 
